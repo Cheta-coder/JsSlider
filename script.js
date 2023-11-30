5a0ab9342392c96f2c9180ee0b4610818f2adcd2
Cheta-coder <happinessugwuanyi7@gmail.com>
2023-11-30 16:04:58 +0100

    another commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let i = 0;
+let j = 0;
 let images = ['IMG/Bengal 5.jpg', 'IMG/Bengal 6.jpg', 'IMG/Pink 2 Siamese.jpg', 'IMG/Siamese bicycle 3.jpg', 'IMG/Sphynx 2.jpg'];
 let interval;
 let slide = document.querySelector('img');
 let leftBtn = document.getElementById('left');
 let rightBtn = document.getElementById('right');
-j = images.length -1
 
 let changeImg = ()=>{
     slide.src = images[i]
@@ -22,26 +22,34 @@ let slideTime = ()=>{
     }, 3000)
 }
 changeImg()
-// slideTime()
+slideTime()
 leftBtn.addEventListener('click',(e)=>{
     // for(let j = images.length -1; j > -1; j--){
-    //     slide.src = images[j]
-    //     console.log(images[j])
+    //     // slide.src = images[j]
     // }
-
-    slide.src = images[i];
-    if(j<images.length -1){
-        i--
-        console.log('ran in if')
+    // let revImg = ()=>{
+    //     clearInterval(interval)
+    //     if(j < images.length -1){
+    //         j--
+    //     }else{
+    //         j = images.length -1
+    //     }
+    // }
+    // revImg()
+    let revImages = images.reverse()
+    slide.src = revImages[j]
+    if(j < revImages.length -1){
+        j++
+        console.log('i am running')
     }else{
-        i = images.length -1
-        console.log('ran in else')
-    }
-
+        j = i;
+        console.log('i ran else')
+    }    
     e.preventDefault()
 })
 
 rightBtn.addEventListener('click',(e)=>{
     changeImg()
+    clearInterval(interval)
     e.preventDefault()
 })
