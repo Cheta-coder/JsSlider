dc83d189e492bc286aafd10f091f1f3c2718cd44
Cheta-coder <happinessugwuanyi7@gmail.com>
2023-12-02 15:39:22 +0100

    a commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Thought Process
+// Grab the src of all the images to be used 
+// Grab the left and right buttons from the html
+// Grab the image element from the html
+// Writing inside a function(changeImg), set the src of the img to a local variable [i]
+// Using if/else statement, check if [i] is less than the number of index present in the array
+// If true, increment [i++]
+// Else let the [i] be the first index
+// For an automatic slide, write a setInterval code and run the function(changeImg)
+// For the left(backward) and right(forward) buttons;
+// Add eventListeners of 'click' for the forword button and run the changeImg() function
+// Add eventlisterners of 'click' for the backward button and decrement [i]
 let i = 0;
 let j = 0;
 let images = ['IMG/Bengal 5.jpg', 'IMG/Bengal 6.jpg', 'IMG/Pink 2 Siamese.jpg', 'IMG/Siamese bicycle 3.jpg', 'IMG/Sphynx 2.jpg'];
@@ -51,6 +63,7 @@ leftBtn.addEventListener('click',(e)=>{
 
 rightBtn.addEventListener('click',(e)=>{
     changeImg()
+    clearInterval(interval)
     e.preventDefault()
 })
 // rightBtn.addEventListener('mouseenter', clearInterval(interval))
